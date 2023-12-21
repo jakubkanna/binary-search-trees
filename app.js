ff6a6c8786d6de7234af096c620aaafb7496bd4a
Jakub Kanna <hello.jakubkanna@gmail.com>
2023-12-21 16:33:03 +0100

    deletes node with one or no children

--- app.js
+++ app.js
@@ -13,17 +13,14 @@ class Tree {
   }
 
   buildTree(arr) {
-    // Implementation of building a balanced binary tree
     //base case
     if (arr.length === 0) return null;
     //init
     const mid = Math.floor(arr.length / 2); //arr must be sorted
     const node = new Node(arr[mid]);
 
-    //recursively:
-    //1. calc left subarr mid and make it root of left subtree
+    //make left and right
     node.left = this.buildTree(arr.slice(0, mid));
-    //2. calc right subarr mid and make it root of right subtree
     node.right = this.buildTree(arr.slice(mid + 1));
 
     // Set root to the level-0 root node
@@ -31,10 +28,10 @@ class Tree {
   }
 
   insert(value, node = this.root) {
-    //base case (leaf)
+    //base case (node.left or node.right changes from null to a new node)
     if (node === null) return new Node(value);
 
-    //go left if true else go right
+    //traverse
     node.data < value
       ? (node.right = this.insert(value, node.right))
       : (node.left = this.insert(value, node.left));
@@ -42,10 +39,20 @@ class Tree {
     return node;
   }
 
-  // Check the value to be inserted (say X) with the value of the current node (say val) we are in
-  //Once the leaf node is reached, insert X to its right or left based on the relation between X and the leaf node’s value.
-
-  delete(value) {}
+  delete(value, node = this.root) {
+    //base case
+    if (node === null) return node;
+    //traverse
+    if (value < node.data) {
+      node.left = this.delete(value, node.left);
+    } else if (value > node.data) {
+      node.right = this.delete(value, node.right);
+    } else {
+      if (node.right == null) return node.left;
+      if (node.left == null) return node.right;
+    }
+    return node;
+  }
 }
 
 const prettyPrint = (node, prefix = "", isLeft = true) => {
@@ -61,8 +68,9 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
   }
 };
 
-const tree = new Tree([100, 20, 500, 10, 30]);
+const tree = new Tree([50, 30, 70, 20, 40, 60, 80]);
 
-tree.insert(29);
+// tree.insert(29);
+tree.delete(20);
 
 prettyPrint(tree.root);
